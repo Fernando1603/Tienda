632dbddfbcd955e425f52fba7f5b25d125058b9c
Fernando <Fernando@DESKTOP-RUGQT4Q.mshome.net>
2023-04-02 17:44:24 -0600

    Commit Fernando. Clase 11

--- src/main/java/com/tienda/domain/Articulo.java
+++ src/main/java/com/tienda/domain/Articulo.java
@@ -4,12 +4,13 @@
  */
 package com.tienda.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
+import javax.persistence.Entity;
 import lombok.Data;
 
 /**
