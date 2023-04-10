5ea7ca06636cce39d3a6f16a7c4facd30466656f
Fernando <Fernando@DESKTOP-RUGQT4Q.mshome.net>
2023-04-10 16:48:20 -0600

    Commit clase 12

--- src/main/java/com/tienda/domain/Articulo.java
+++ src/main/java/com/tienda/domain/Articulo.java
@@ -4,13 +4,13 @@
  */
 package com.tienda.domain;
 
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.io.Serializable;
-import javax.persistence.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import lombok.Data;
 
 /**
@@ -19,9 +19,10 @@ import lombok.Data;
  */
 @Data
 @Entity
-@Table(name="articulo")
+@Table(name = "articulo")
 
-public class Articulo implements Serializable{
+public class Articulo implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idArticulo; // Transforma en id_cliente
@@ -31,22 +32,19 @@ public class Articulo implements Serializable{
     private double precio;
     private int existencias;
     private boolean activo;
+    private String rutaImagen;
 
     public Articulo() {
     }
 
-    public Articulo(Long idArticulo, Long idCategoria, String descripcion, String detalle, double precio, int existencias, boolean activo) {
-        this.idArticulo = idArticulo;
+    public Articulo(Long idCategoria, String descripcion, String detalle, double precio, int existencias, boolean activo, String rutaImagen) {
         this.idCategoria = idCategoria;
         this.descripcion = descripcion;
         this.detalle = detalle;
         this.precio = precio;
         this.existencias = existencias;
         this.activo = activo;
+        this.rutaImagen = rutaImagen;
     }
 
-    
-    }
-    
-    
-
+}
